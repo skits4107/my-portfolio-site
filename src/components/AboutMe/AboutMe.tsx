231b7f952bcbbc2a8921be203e0ba3815d07d247
skits4107 <74423228+skits4107@users.noreply.github.com>
2025-10-31 14:51:13 -0700

    added name

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -7,11 +7,14 @@ function AboutMe(){
     return (
         <div className="AboutContainer">
             <img src={PhotoOfme} className="ProfilePhoto"/>
-            <p className="aboutText">
-                I am a versitile freelance software developer who can make anything <br></br>
-                from minecraft mods to react website pages. I am able to do so much thanks to <br></br>
-                my many years teaching kids how to code at The Coder School. <br></br>
-            </p>
+            <span>
+                <h3 className="nameHeader">Aaron Weinberger</h3>
+                <p className="aboutText">
+                    I am a versitile freelance software developer who can make anything <br></br>
+                    from minecraft mods to react website pages. I am able to do so much thanks to <br></br>
+                    my many years teaching kids how to code at The Coder School. <br></br>
+                </p>
+            </span>
         </div>
     );
 }

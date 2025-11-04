1e75383393315adb0eb0a9b1bb7c34a1542a0c05
skits4107 <aaronjosua719@gmail.com>
2025-11-03 17:41:54 -0800

    made nav buttons change route

--- src/components/NavigationMenu.tsx/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx/NavigationMenu.tsx
@@ -1,19 +1,25 @@
 
 import "./NavigationMenu.css"
+ import { useNavigate } from 'react-router-dom';
 
 function NavigationMenu(){
+    const navigate = useNavigate();
+
+    const changePage = (page:string) => {
+        navigate('/'+page); // Navigate to /dashboard
+    };
 
     return (
         <div className="NavContainer">
             <ol className="NavList">
                 <li className="NavItem">
-                    <button className="NavBtn" key="homeBtn">Home</button>
+                    <button className="NavBtn" key="homeBtn" onClick={()=>{changePage("")}}>Home</button>
                 </li>
                 <li>
-                    <button className="NavBtn" key="aboutBtn">About</button>
+                    <button className="NavBtn" key="aboutBtn" onClick={()=>{changePage("about")}}>About</button>
                 </li>
                 <li>
-                    <button className="NavBtn" key="contactBtn">Contact</button>
+                    <button className="NavBtn" key="contactBtn" onClick={()=>{changePage("contact")}}>Contact</button>
                 </li>
             </ol>
         </div>

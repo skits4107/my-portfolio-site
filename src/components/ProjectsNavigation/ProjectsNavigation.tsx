4ccf4f122d309f48c88a1cbc452d049763a4d0d7
skits4107 <aaronjosua719@gmail.com>
2025-11-03 17:29:35 -0800

    added project images

--- src/components/ProjectsNavigation/ProjectsNavigation.tsx
+++ src/components/ProjectsNavigation/ProjectsNavigation.tsx
@@ -1,15 +1,17 @@
 import "./ProjectsNavigation.css"
 import ProjectBtn from "../ProjectBtn/ProjectBtn";
 import react from "../../assets/react.svg";
-
+import minecraftLogo from "../../assets/minecraftLogo.png"
+import python from "../../assets/python.png"
+import cppLogo from "../../assets/cppLogo.svg"
 
 function ProjectsNavigation(){
     return (
         <div className="ProjectsNavContainer">
-            <ProjectBtn src={react} text="react"/>
-            <ProjectBtn src={react} text="react"/>
-            <ProjectBtn src={react} text="react"/>
-            <ProjectBtn src={react} text="react"/>
+            <ProjectBtn src={react} text="React"/>
+            <ProjectBtn src={minecraftLogo} text="Mods"/>
+            <ProjectBtn src={python} text="Python"/>
+            <ProjectBtn src={cppLogo} text="C++"/>
         </div>
     );
 }

9f5dbeb80fb5fc95d5671ce4009fc73f9d08ad9f
skits4107 <aaronjosua719@gmail.com>
2025-11-03 16:57:39 -0800

    Got basic project navigatio ndone

--- src/components/ProjectsNavigation/ProjectsNavigation.tsx
+++ src/components/ProjectsNavigation/ProjectsNavigation.tsx
@@ -1,7 +1,17 @@
+import "./ProjectsNavigation.css"
+import ProjectBtn from "../ProjectBtn/ProjectBtn";
+import react from "../../assets/react.svg";
 
 
 function ProjectsNavigation(){
-
+    return (
+        <div className="ProjectsNavContainer">
+            <ProjectBtn src={react} text="react"/>
+            <ProjectBtn src={react} text="react"/>
+            <ProjectBtn src={react} text="react"/>
+            <ProjectBtn src={react} text="react"/>
+        </div>
+    );
 }
 
 export default ProjectsNavigation;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "../components/AboutMe/AboutMe.tsx"
 import NavigationMenu from "../components/NavigationMenu.tsx/NavigationMenu.tsx";
+import ProjectsNavigation from "../components/ProjectsNavigation/ProjectsNavigation.tsx";
 
 function HomePage(){
     return (
         <>
             <NavigationMenu />
             <AboutMe />
+            <ProjectsNavigation />
         </>
     )
 }
